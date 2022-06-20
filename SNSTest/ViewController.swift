4c1a7654d0066d0a665408e8e5298fc7c0d08020
YukiSekido <step1336@gmail.com>
2022-06-20 20:01:50 +0900

    Initial Commit

---
+++ SNSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SNSTest
+//
+//  Created by 関戸優紀 on 2022/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
